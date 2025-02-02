8a7e16648a9dd23dd943e2f4a31974a22cde4715
1012abhi <130040653+1012abhi@users.noreply.github.com>
2025-02-02 13:02:55 +0530

    refactor Card component layout for improved styling and alignment

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,13 +9,13 @@ function Card({title,live,Img}) {
                 <img 
                     className=' ml-12 mt-24 max-sm:ml-12 max-sm:mt-20  max-w-[300px] h-[100px] rounded-3xl bg-white shadow-lg hover:scale-95 translate-x-0 shadow-blue-500/50 md:w-[250px] md:h-[140px] lg:w-[250px] lg:h-[150px] transition-all '
                     src={Img} alt="" />
-                <div className=" absolute p-6 top-0">
+                <div className=" absolute p-6 top-0 flex justify-center w-full">
                     <h2 className="text-xl font-bold text-blue-600 mb-2 max-sm:text-lg max-sm:mt-0">{title}</h2>
                 </div>
                 <div className='flex'>
-                        <p className='ml-[22px] mt-[38%] max-sm:mt-0 max-sm:text-sm bg-gray-600 w-fit h-fit pb-1 px-1 text-zinc-100'>React.Js</p>
+                    <p className='ml-[22px] mt-[38%] max-sm:mt-0 max-sm:text-sm bg-gray-600 w-fit h-fit pb-1 px-1 text-zinc-100'>React.Js</p>
                         
-                    </div>
+                </div>
                 <Link
                     to={live}
                     className="absolute flex flex-col bottom-5 right-6 px-4 py-1 border-1 text-white bg-black border-black rounded-full font-semibold shadow-md shadow-blue-600/50 hover:bg-blue-700 hover:shadow-lg hover:shadow-blue-800/70 transition-all duration-300"
